50f9c859974054d6d79064c564f5b98be4537ff8
Deepika <deepikardb@gmail.com>
2024-01-20 11:49:38 -0800

    Initial Commit

---
+++ FetchDessert/FetchDessertApp.swift
@@ -0,0 +1,19 @@
+//
+//  FetchDessertApp.swift
+//  FetchDessert
+//
+//  Created by Deepika Ramesh on 1/20/24.
+//
+
+
+
+import SwiftUI
+
+@main
+struct FetchDessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
